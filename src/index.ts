b7d6b745a5e833ce87ff99962461e3008fb23441
Sammy Ferreira <sammyfe2021@gmail.com>
2024-01-26 14:09:26 -0300

    :sparkles: feat: cors

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,18 @@ import express from "express";
 import { AppDataSource } from "./data-source";
 import { errorMiddleware } from "./middlewares/error";
 import routes from "./routes";
+import cors from 'cors'
 
 AppDataSource.initialize().then(() => {
   const app = express()
+
+  app.use(cors())
 
   app.use(express.json())
 
   app.use(routes)
 
   app.use(errorMiddleware)
-
-  app.get('/', (req, res) => {
-    res.send('Tudo Certo!')
-  })
 
   return app.listen(process.env.PORT)
 });
