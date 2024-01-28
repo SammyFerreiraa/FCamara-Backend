d08ebd4711fbfcab311233372003b49e7fc0f761
Sammy Ferreira <sammyfe2021@gmail.com>
2024-01-28 15:25:47 -0300

    :sparkles: feat: rentals and get user router

--- src/routes.ts
+++ src/routes.ts
@@ -3,15 +3,21 @@ import { UserController } from './controllers/UserController'
 import { LoginController } from './controllers/LoginController'
 import { BooksController } from './controllers/BooksController'
 import { CopyController } from './controllers/CopyController'
+import { RentalsController } from './controllers/RentalsController'
+import { authMiddleware } from './middlewares/authMiddleware'
 
 const routes = Router()
 
 routes.post('/users', new UserController().create)
+routes.get('/users', authMiddleware, new UserController().getUser)
+
 routes.post('/login', new LoginController().login)
 
 routes.post('/books', new BooksController().create)
 routes.get('/books', new BooksController().list)
 
 routes.post('/copies', new CopyController().create)
+
+routes.post('/rentals', authMiddleware ,new RentalsController().create)
 
 export default routes
