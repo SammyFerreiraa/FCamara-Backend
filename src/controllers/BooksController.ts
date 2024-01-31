c7e1a190d5c9248898dee38774ef01bf07411aee
Sammy Ferreira <sammyfe2021@gmail.com>
2024-01-31 12:17:50 -0300

    :sparkles: feat: delete functionality to BooksController

--- src/controllers/BooksController.ts
+++ src/controllers/BooksController.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express"
 import { BooksRepository } from "../repositories/booksRepository"
+import { BookRepository } from "../repositories/bookRepository"
+import { CopyRepository } from "../repositories/copyRepository"
+import { RentalRepository } from "../repositories/rentalRepository"
 
 export class BooksController {
   async create(req: Request, res: Response) {
@@ -42,4 +45,29 @@ export class BooksController {
       console.log(error)
     }
   }
+
+  async delete(req: Request, res: Response) {
+    try {
+      const id = req.params.id
+
+      const book = await BooksRepository.findOne({ where: { id } })
+      if (!book) return res.status(400).json({'message': 'Book not found'})
+
+      const copy = await CopyRepository.find({ where: { book } })
+      if (!copy) return res.status(400).json({'message': 'Copy not found'})
+
+      await BookRepository.delete({ isbn: book.isbn })
+      
+      copy.map(async (copy) => {
+        await RentalRepository.delete({ copy })
+      })
+
+      await CopyRepository.delete({ book })
+      await BooksRepository.delete({ id })
+
+      return res.status(200).json({'message': 'Book deleted'})
+    } catch (error) {
+      console.log(error)
+    }
+  } 
 }
