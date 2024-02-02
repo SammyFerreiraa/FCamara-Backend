54d766b3a74c00e16954ee672bbb0e2bf838e45a
Sammy Ferreira <sammyfe2021@gmail.com>
2024-02-02 16:11:22 -0300

    :recycle: refactor: user registration route and protect routes with middleware

--- src/routes.ts
+++ src/routes.ts
@@ -15,24 +15,29 @@ const copyController = new CopyController()
 const rentalsController = new RentalsController()
 
 routes.post('/login', loginController.login) // Login route
+routes.post('/register', userController.create) // Create user
 
-routes.post('/users', userController.create) // Create user
-routes.get('/users', authMiddleware, userController.getUser) // Get user
-routes.put('/users', authMiddleware, userController.updateUser) // Update user
-routes.delete('/users', authMiddleware, userController.deleteUser) // Delete user
+routes.use('/users', authMiddleware) // Middleware to protect routes
+routes.get('/users', userController.getUser) // Get user
+routes.put('/users', userController.updateUser) // Update user
+routes.delete('/users', userController.deleteUser) // Delete user
 
-routes.post('/books', authMiddleware, booksController.create) // Create book
+routes.use('/books', authMiddleware) // Middleware to protect routes
+routes.post('/books', booksController.create) // Create book
 routes.get('/books', booksController.listAll) // List all books
-routes.put('/books/:id', authMiddleware, booksController.update) // Update book
-routes.delete('/books/:id', authMiddleware, booksController.delete) // Delete book
+routes.put('/books/:id', booksController.update) // Update book
+routes.delete('/books/:id', booksController.delete) // Delete book
 
-routes.post('/copies', authMiddleware, copyController.create) // Create copy
+routes.use('/copies', authMiddleware) // Middleware to protect routes
+routes.post('/copies', copyController.create) // Create copy
 routes.get('/copies', copyController.listAll) // List all copies
-routes.delete('/copies/:id', authMiddleware, copyController.delete) // Delete copy
+routes.delete('/copies/:id', copyController.delete) // Delete copy
 
-routes.post('/rentals', authMiddleware ,rentalsController.create) // Create rental
-routes.delete('/rentals/:id', authMiddleware, rentalsController.returnBook) // Return book
+routes.use('/rentals', authMiddleware) // Middleware to protect routes
+routes.post('/rentals' ,rentalsController.create) // Create rental
+routes.delete('/rentals/:id', rentalsController.returnBook) // Return book
 
-routes.get('/delay', authMiddleware, booksController.delayedBooks) // List all delayed books
+routes.use('/delay', authMiddleware) // Middleware to protect routes
+routes.get('/delay', booksController.delayedBooks) // List all delayed books
 
 export default routes
